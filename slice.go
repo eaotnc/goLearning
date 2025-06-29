0416c5c8d206e4bf3b8378e9273eab64311ea50c
Thanatcha (Eao) Pitihadakul <thanatcha@robowealth.co.th>
2025-06-29 23:51:21 +0700

    update slice.go to demonstrate creating an empty slice and handling potential runtime errors

--- slice.go
+++ slice.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main(){
-
+     
 	// create slice of user lating for move in 2 slices 
 	var userRatings = []int{5, 4, 3, 2, 1}
 	var userRatings2 = []int{1, 2, 3, 4, 5}
@@ -17,4 +17,9 @@ func main(){
 	// show combined ratings 5-8 using slice syntax
 	fmt.Printf("combinedRatings[5:8]: %v\n", combinedRatings[5:8])
 
+
+	// create empty slice of strings
+	var emptySlice = make([]string,3)
+	emptySlice[1] = "Hello" // This will cause a runtime error because the slice is empty and has no capacity
+	fmt.Printf("emptySlice: %v\n", emptySlice)
 }
